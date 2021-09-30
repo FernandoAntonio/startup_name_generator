a33b8cc04afe06907ca24a9caac8b135203ebd28
Fernando Antonio <30207066+fernandoantonio@users.noreply.github.com>
2021-09-29 22:29:25 -0300

    Added keystore dependency

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Startup Name Generator',
       theme: ThemeData(primaryColor: Colors.green[900], splashColor: Colors.grey),
       home: RandomWords(),
